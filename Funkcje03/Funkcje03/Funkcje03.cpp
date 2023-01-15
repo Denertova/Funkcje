9df9f20ddabda72e750ad6a341953487dcbb344f
Denertova <122315020+Denertova@users.noreply.github.com>
2023-01-15 03:05:33 +0100

    Add files via upload

---
+++ Funkcje03/Funkcje03/Funkcje03.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct student {
+	string imie;
+	string nazwisko;
+	int numerindeksu;
+};
+
+int main()
+{
+	//Napisz program do sortowania wektora struktur studentów według numerów indeksów. 
+	//Struktura Student ma zawierać informacje o: imieniu, nazwisku i numerze indeksu.
+
+	vector<student> stud = { {"Kasia", "Kowalska", 78797},
+							 {"Jacek", "Wroblewski", 67845},
+							 {"Bozena", "Trysuk", 79976} };
+	return 0;
+}
